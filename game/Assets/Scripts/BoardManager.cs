0e5184472980e5f1db99fbc4d824e3f7b0298bf4
oscar galera alfaro <oscar.galeraa@gmail.com>
2017-11-01 21:59:56 +0100

    pendent

--- game/Assets/Scripts/BoardManager.cs
+++ game/Assets/Scripts/BoardManager.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class BoardManager : MonoBehaviour {
+	public GameObject sortides;
+	public GameObject jugador;
+	public GameObject paretOutH;
+	public GameObject paretOutV;
+	public GameObject paretIn;
+	public GameObject[] monedes;
 
-	public GameObject exit;
+	private Transform boardHolder;
+	private List<List<Vector2>> taulell = null;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	public int nFiles;
+	public int nColumnes;
+
+	private void iniciar()
+	{
+		if (taulell == null)
+		{
+			taulell = new List<List<Vector2>>(nFiles);
+		}
+		taulell.Clear();
+		/*for (int i = 1; i < nFiles - 1; i++){
+			taulell.
+		}*/
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void carregar(string fitxer)
+	{
+
+		using (StreamReader stream = new StreamReader(fitxer, Encoding.Default))
+		{
+			string linia = null;
+			while ((linia = stream.ReadLine()) != null)
+			{
+
+			}
+		}
 	}
 }
